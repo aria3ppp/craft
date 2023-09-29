e309ac27fb96d2667ab82194bf417cb7d235e646
Aria Paryab <aria3ppp@gmail.com>
2023-09-29 17:25:17 +0330

    fix multiple macro function invocation bug

--- cmd/craft/craft.go
+++ cmd/craft/craft.go
@@ -56,93 +56,99 @@ func main() {
 		if genDecl, ok := decl.(*ast.GenDecl); ok && genDecl.Tok == token.TYPE {
 
 			var (
-				macroFunctionName string
-				startOffset       = fs.Position(genDecl.Pos()).Offset
-				endOffset         = fs.Position(genDecl.End()).Offset
+				startOffset = fs.Position(genDecl.Pos()).Offset
+				endOffset   = fs.Position(genDecl.End()).Offset
 			)
+
+			macroFunctionInvocations := map[string]struct{}{}
 
 			if genDecl.Doc != nil {
 				for _, comment := range genDecl.Doc.List {
 					if submatches := macroTagRegexp.FindStringSubmatch(comment.Text); len(submatches) > 1 {
-						macroFunctionName = submatches[1]
+						macroFunctionName := submatches[1]
+						macroFunctionInvocations[macroFunctionName] = struct{}{}
 					}
 				}
 			}
 
-			for _, spec := range genDecl.Specs {
-				if typeSpec, ok := spec.(*ast.TypeSpec); ok {
+			for macroFunctionName, _ := range macroFunctionInvocations {
 
-					needTypePrepend := false
-					typeSpecName := typeSpec.Name.Name
+				for _, spec := range genDecl.Specs {
+					if typeSpec, ok := spec.(*ast.TypeSpec); ok {
 
-					// if it was a grouped multiple
-					if len(genDecl.Specs) > 1 {
-						startOffset = fs.Position(typeSpec.Pos()).Offset
-						endOffset = fs.Position(typeSpec.End()).Offset
+						needTypePrepend := false
+						typeSpecName := typeSpec.Name.Name
 
-						macroFunctionName = ""
+						// if it was a grouped multiple
+						if len(genDecl.Specs) > 1 {
+							startOffset = fs.Position(typeSpec.Pos()).Offset
+							endOffset = fs.Position(typeSpec.End()).Offset
 
-						if typeSpec.Doc != nil {
-							for _, comment := range typeSpec.Doc.List {
-								if submatches := macroTagRegexp.FindStringSubmatch(comment.Text); len(submatches) > 1 {
-									macroFunctionName = submatches[1]
+							macroFunctionName = ""
+
+							if typeSpec.Doc != nil {
+								for _, comment := range typeSpec.Doc.List {
+									if submatches := macroTagRegexp.FindStringSubmatch(comment.Text); len(submatches) > 1 {
+										macroFunctionName = submatches[1]
+									}
 								}
 							}
+
+							if macroFunctionName == "" {
+								continue
+							}
+
+							needTypePrepend = true
 						}
 
-						if macroFunctionName == "" {
-							continue
+						// TODO: spawn a new craftMaterials:
+						// 1. there would be more than one macro invocation
+						// 2. to run in parallel
+
+						macroOutputFilename := buildFilename(typeSpecName, macroPackageImportPath, macroFunctionName) + ".gen.go"
+
+						programString, err := generateProgram(
+							Values{
+								Macro: Macro{
+									Name:              macroFunctionName,
+									PackageImportPath: macroPackageImportPath,
+								},
+								Source: Source{
+									Filepath: filepath.Join(currentWorkingDirectory, currentFilename),
+								},
+								GenDecl: GenDecl{
+									StartOffset: startOffset,
+									EndOffset:   endOffset,
+								},
+								Output: Output{
+									Filepath: filepath.Join(currentWorkingDirectory, macroOutputFilename),
+								},
+								Template: Template{
+									TypeSpecName: typeSpecName,
+									PackageName:  file.Name.Name,
+								},
+								NeedTypePrepend: needTypePrepend,
+							},
+						)
+						if err != nil {
+							fmt.Printf("error: failed generating program: %s\n", err)
+							os.Exit(5)
+							return
 						}
 
-						needTypePrepend = true
+						if err := craftMaterials(
+							convertPathToFilename(currentWorkingDirectory),
+							macroOutputFilename,
+							programString,
+						); err != nil {
+							fmt.Println(err.Error())
+							os.Exit(6)
+							return
+						}
+
 					}
-
-					// TODO: spawn a new craftMaterials:
-					// 1. there would be more than one macro invocation
-					// 2. to run in parallel
-
-					macroOutputFilename := buildFilename(typeSpecName, macroPackageImportPath, macroFunctionName) + ".gen.go"
-
-					programString, err := generateProgram(
-						Values{
-							Macro: Macro{
-								Name:              macroFunctionName,
-								PackageImportPath: macroPackageImportPath,
-							},
-							Source: Source{
-								Filepath: filepath.Join(currentWorkingDirectory, currentFilename),
-							},
-							GenDecl: GenDecl{
-								StartOffset: startOffset,
-								EndOffset:   endOffset,
-							},
-							Output: Output{
-								Filepath: filepath.Join(currentWorkingDirectory, macroOutputFilename),
-							},
-							Template: Template{
-								TypeSpecName: typeSpecName,
-								PackageName:  file.Name.Name,
-							},
-							NeedTypePrepend: needTypePrepend,
-						},
-					)
-					if err != nil {
-						fmt.Printf("error: failed generating program: %s\n", err)
-						os.Exit(5)
-						return
-					}
-
-					if err := craftMaterials(
-						convertPathToFilename(currentWorkingDirectory),
-						macroOutputFilename,
-						programString,
-					); err != nil {
-						fmt.Println(err.Error())
-						os.Exit(6)
-						return
-					}
-
 				}
+
 			}
 
 		}
